2d1e4bc2c58c0266d3fcef7156edad3536c8e4fe
Roman Telik <rtelik58@gmail.com>
2023-06-03 12:10:44 +0300

    pt2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
 console.log(galleryItems);
+let gallerySubArr = [];
+galleryItems.forEach(function (item) {
+  let card = `
+<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`;
+  gallerySubArr.push(card);
+});
+gallery.innerHTML = gallerySubArr.join('');
+const delegation = event => {
+  event.preventDefault();
+};
+gallery.addEventListener('click', delegation);
