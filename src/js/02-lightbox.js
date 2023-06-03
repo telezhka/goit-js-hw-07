12458c70e42188f195668c2ae3228073b8bf4e38
Roman Telik <rtelik58@gmail.com>
2023-06-03 12:51:38 +0300

    done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -7,13 +7,24 @@ let gallerySubArr = [];
 galleryItems.forEach(function (item) {
   let card = `
 <li class="gallery__item">
-   <a class="gallery__link" href="${item.original}">
-      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   <a class="gallery__link" href="${item.original}" data-caption="${item.description}">
+      <img
+      class="gallery__image"
+      src="${item.preview}" 
+      alt="${item.description}"
+      title="${item.description}" />
    </a>
 </li>`;
   gallerySubArr.push(card);
 });
 gallery.innerHTML = gallerySubArr.join('');
+// import SimpleLightbox from 'simplelightbox'; не працює
+// import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm'; не працює
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captions: true,
+  captionDelay: 250,
+});
 const delegation = event => {
   event.preventDefault();
 };
